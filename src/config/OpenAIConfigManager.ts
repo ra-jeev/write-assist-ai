af0639a154894a1bd29afd24e0b80b9b676cf7b3
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-06 23:55:23 +0530

    feat: create standalone OpenAIConfigManager

---
+++ src/config/OpenAIConfigManager.ts
@@ -0,0 +1,147 @@
+import { ConfigurationChangeEvent } from 'vscode';
+import { ExtensionConfig } from './ExtensionConfig';
+import { SecretsManager } from './SecretsManager';
+import { isConfigChanged, promptUserForConfig } from './ConfigUtils';
+import {
+  ConfigurationKeys,
+  DEFAULT_MAX_TOKENS,
+  DEFAULT_TEMPERATURE,
+  DEFAULT_MODEL,
+} from '../constants';
+import type { OpenAIConfig } from '../types';
+
+export class OpenAIConfigManager {
+  private changeListener: ((resetOpenAiSvc: boolean) => any) | undefined;
+
+  constructor(
+    private readonly config: ExtensionConfig,
+    private readonly secretsManager: SecretsManager
+  ) {
+    this.secretsManager.registerChangeListener(
+      ConfigurationKeys.openAiApiKey,
+      this.onApiKeyChanged.bind(this)
+    );
+  }
+
+  async getApiKey(): Promise<string | undefined> {
+    return this.secretsManager.getSecret(ConfigurationKeys.openAiApiKey);
+  }
+
+  async promptUserForApiKey(): Promise<string | undefined> {
+    const apiKey = await promptUserForConfig({
+      title: 'Your OpenAI API Key',
+      prompt: 'Enter your API key here',
+      placeHolder: 'sk-xxxxxxxxxxxxxxxx',
+      password: true,
+      ignoreFocusOut: true,
+    });
+
+    // user might enter empty string to reset the key, so check against undefined
+    if (apiKey === undefined) {
+      console.log('No API key provided by user');
+      return;
+    }
+
+    await this.secretsManager.storeSecret(
+      ConfigurationKeys.openAiApiKey,
+      apiKey
+    );
+
+    return apiKey;
+  }
+
+  async migrateApiKey(): Promise<void> {
+    const apiKey = this.config.getConfiguration<string>(
+      ConfigurationKeys.deprecatedOpenAiApiKey,
+      ''
+    );
+
+    if (apiKey.default) {
+      // Store the key in the secretStorage
+      await this.secretsManager.storeSecret(
+        ConfigurationKeys.openAiApiKey,
+        apiKey.default
+      );
+
+      // Delete the key from the settings
+      await this.config.updateConfiguration(
+        ConfigurationKeys.deprecatedOpenAiApiKey,
+        undefined
+      );
+    }
+  }
+
+  getConfig(): OpenAIConfig {
+    const maxTokens = this.config.getConfiguration<number>(
+      ConfigurationKeys.maxTokens,
+      DEFAULT_MAX_TOKENS
+    );
+
+    const temperature = this.config.getConfiguration<number>(
+      ConfigurationKeys.temperature,
+      DEFAULT_TEMPERATURE
+    );
+
+    const systemPrompt = this.config.getConfiguration<string>(
+      ConfigurationKeys.systemPrompt,
+      ''
+    );
+
+    let model = this.config.getConfiguration<string>(
+      ConfigurationKeys.model,
+      DEFAULT_MODEL
+    );
+
+    if (model.default === 'custom') {
+      model = this.config.getConfiguration<string>(
+        ConfigurationKeys.customModel,
+        DEFAULT_MODEL
+      );
+
+      if (!model.default) {
+        model.default = DEFAULT_MODEL;
+      }
+    }
+
+    return {
+      maxTokens: maxTokens.default,
+      model: model.default,
+      temperature: temperature.default,
+      systemPrompt,
+    };
+  }
+
+  getProxyUrl(): string | undefined {
+    return this.config.getConfiguration<string>(ConfigurationKeys.proxyUrl, '')
+      .default;
+  }
+
+  hasConfigChanged(event: ConfigurationChangeEvent): boolean {
+    return (
+      isConfigChanged(event, ConfigurationKeys.model) ||
+      isConfigChanged(event, ConfigurationKeys.customModel) ||
+      isConfigChanged(event, ConfigurationKeys.maxTokens) ||
+      isConfigChanged(event, ConfigurationKeys.temperature) ||
+      isConfigChanged(event, ConfigurationKeys.systemPrompt) ||
+      isConfigChanged(event, ConfigurationKeys.proxyUrl)
+    );
+  }
+
+  notifyConfigChanged(event: ConfigurationChangeEvent) {
+    if (this.changeListener) {
+      const resetOpenAISvc = isConfigChanged(event, ConfigurationKeys.proxyUrl);
+
+      this.changeListener(resetOpenAISvc);
+    }
+  }
+
+  registerChangeListener(listener: (resetOpenAISvc: boolean) => any) {
+    this.changeListener = listener;
+  }
+
+  private onApiKeyChanged(key: string) {
+    if (this.changeListener && key === ConfigurationKeys.openAiApiKey) {
+      this.changeListener(true);
+    }
+  }
+}
