63b2905c9b3f8c8707dd036033eae8cd95b68885
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-06 23:56:25 +0530

    feat: standalone ActionsConfigManager

---
+++ src/config/ActionsConfigManager.ts
@@ -0,0 +1,39 @@
+import { ConfigurationChangeEvent } from 'vscode';
+import { ExtensionConfig } from './ExtensionConfig';
+import { isConfigChanged } from './ConfigUtils';
+import { ConfigurationKeys } from '../constants';
+import type { ExtensionActions, LanguageConfig, WritingAction } from '../types';
+
+export class ActionsConfigManager {
+  constructor(private readonly config: ExtensionConfig) {}
+
+  getActions(): ExtensionActions {
+    return {
+      quickFixes: this.getActionsType(ConfigurationKeys.quickFixes),
+      rewriteActions: this.getActionsType(ConfigurationKeys.rewriteOptions),
+    };
+  }
+
+  private getActionsType(type: string): LanguageConfig<WritingAction[]> {
+    const actionsCfg = this.config.getConfiguration<
+      Omit<WritingAction, 'id'>[]
+    >(type, []);
+
+    const actions: LanguageConfig<WritingAction[]> = { default: [] };
+    for (const languageId in actionsCfg) {
+      actions[languageId] = actionsCfg[languageId].map((action, index) => {
+        const id = `${type.toLowerCase()}-${languageId}-${index}`;
+        return { ...action, id, prompt: action.prompt.trim() };
+      });
+    }
+
+    return actions;
+  }
+
+  hasConfigChanged(event: ConfigurationChangeEvent): boolean {
+    return (
+      isConfigChanged(event, ConfigurationKeys.quickFixes) ||
+      isConfigChanged(event, ConfigurationKeys.rewriteOptions)
+    );
+  }
+}
