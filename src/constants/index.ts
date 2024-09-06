54df2726a6dd6c665a65ff6a0edbe30c781290c8
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-06 23:53:31 +0530

    feat: add separate constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,30 @@
+export const DEFAULT_MAX_TOKENS = 1200;
+export const DEFAULT_TEMPERATURE = 0.3;
+export const DEFAULT_MODEL = 'gpt-4o';
+
+export const ERROR_MESSAGES = {
+  NO_API_KEY: 'No API key provided.',
+  FAILED_TO_PROCESS: 'Error: Failed to process.',
+  NO_CHOICES_RETURNED: 'Warning: No choices returned by the API.',
+  EMPTY_CONTENT: 'Warning: Empty content received from the API.',
+};
+
+// Keys for the extensions configuration settings
+export enum ConfigurationKeys {
+  deprecatedOpenAiApiKey = 'openAiApiKey',
+  openAiApiKey = 'openAi.apiKey',
+  maxTokens = 'maxTokens',
+  temperature = 'temperature',
+  model = 'openAi.model',
+  customModel = 'openAi.customModel',
+  proxyUrl = 'openAi.proxyUrl',
+  quickFixes = 'quickFixes',
+  rewriteOptions = 'rewriteOptions',
+  systemPrompt = 'systemPrompt',
+}
+
+// Keys for the VSCode Command Pallette Commands
+// that this extension provides
+export enum CommandKeys {
+  openAiApiKey = 'openAiApiKey',
+}
