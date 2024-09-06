b9505a49453e63eb7d98c533fcd031aed123b208
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-06 23:54:14 +0530

    feat: add separate types file

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export type WritingAction = {
+  id: string;
+  title: string;
+  description: string;
+  prompt: string;
+};
+
+export type ExtensionActions = {
+  quickFixes: LanguageConfig<WritingAction[]>;
+  rewriteActions: LanguageConfig<WritingAction[]>;
+};
+
+export type LanguageConfig<T> = {
+  [key: string]: T;
+  default: T;
+};
+
+export type OpenAIConfig = {
+  model: string;
+  maxTokens: number;
+  temperature: number;
+  systemPrompt: LanguageConfig<string>;
+};
+
+export type SecretChangeListener = (key: string) => void;
