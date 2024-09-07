fdfb2e39640ceb58d2db4a7c7fd9a042e7094521
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-07 14:40:59 +0530

    refactor: main extension file as per new architecture

--- src/extension.ts
+++ src/extension.ts
@@ -1,55 +1,10 @@
-import { commands, ExtensionContext, languages } from 'vscode';
-import { WriteAssistAI } from './writeAssistAI';
-import { ExtensionConfig } from './ExtensionConfig';
+import { ExtensionContext } from 'vscode';
+import { ExtensionManager } from './ExtensionManager';
 
-const handleCommandsConfigChange = (
-  context: ExtensionContext,
-  config: ExtensionConfig
-) => {
-  // Dispose all the existing subscriptions
-  while (context.subscriptions.length) {
-    context.subscriptions.pop()?.dispose();
-  }
-
-  // Register commands and actions afresh
-  registerCommandsAndActions(context, config);
-};
-
-const registerCommandsAndActions = (
-  context: ExtensionContext,
-  config: ExtensionConfig
-) => {
-  const writeAssist: WriteAssistAI = new WriteAssistAI(config);
-  const aiActionProvider = languages.registerCodeActionsProvider(
-    ['markdown', 'plaintext', 'tex', 'latex', 'bibtex'],
-    writeAssist,
-    {
-      providedCodeActionKinds: WriteAssistAI.providedCodeActionKinds,
-    }
-  );
-
-  context.subscriptions.push(aiActionProvider);
-  for (const command of writeAssist.commands) {
-    context.subscriptions.push(
-      commands.registerCommand(command, (args) => {
-        writeAssist.handleAction(args);
-      })
-    );
-  }
-
-  // Add the command for OpenAI API Key configuration
-  context.subscriptions.push(
-    commands.registerCommand(ExtensionConfig.openAiApiKeyCmd, () =>
-      config.promptUserForApiKey()
-    )
-  );
-};
+let extensionManager: ExtensionManager;
 
 export function activate(context: ExtensionContext) {
-  registerCommandsAndActions(
-    context,
-    new ExtensionConfig(context, handleCommandsConfigChange)
-  );
+  extensionManager = new ExtensionManager(context);
 }
 
 export function deactivate() {}
